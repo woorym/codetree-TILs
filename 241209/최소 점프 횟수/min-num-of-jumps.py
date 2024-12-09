f4c934c842bf2a218e7e3ad8209729f5533a1573
WooLim Lee <108933788+woorym@users.noreply.github.com>
2024-12-09 12:00:50 +0900

    Title: 최소 점프 횟수, Time: 80ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241209/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+max_num = sys.maxsize
+
+
+n = int(input())
+num_list = list(map(int, input().split()))
+arr= [max_num]
+cnt = 0
+def again(current, cnt):
+    if current == n - 1:
+        arr.append(cnt) 
+        return
+
+    for i in range(1, num_list[current] + 1):
+        cnt += 1
+        again(current + i, cnt)
+        cnt -= 1
+
+
+
+again(0, 0)
+if len(arr) == 1:
+    print(-1)
+else:
+    print(min(arr))
